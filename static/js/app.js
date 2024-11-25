0ca9ba35083f69ee8b3e213d80f0e41f61ab189e
BagusKrisnas <baguswikramakrisna@gmail.com>
2024-11-25 13:32:14 +0700

    update login page

--- static/js/app.js
+++ static/js/app.js
@@ -14,3 +14,17 @@ const navLinks = document.querySelector('.nav-links');
 menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('show');
 });
+
+function togglePassword() {
+  const passwordField = document.getElementById('password');
+  const toggleIcon = document.querySelector('.toggle-password');
+  const fieldType = passwordField.getAttribute('type');
+
+  if (fieldType === 'password') {
+    passwordField.setAttribute('type', 'text');
+    toggleIcon.src = 'eye-closed-icon.png'; // Image for hiding password
+  } else {
+    passwordField.setAttribute('type', 'password');
+    toggleIcon.src = 'eye-icon.png'; // Image for showing password
+  }
+}
