4b9fb9eb4677341b89d2c9230ca48b917b87b7ef
BagusKrisnas <baguswikramakrisna@gmail.com>
2024-11-27 19:32:45 +0700

    faq question list

--- static/js/app.js
+++ static/js/app.js
@@ -28,3 +28,13 @@ function togglePassword() {
     toggleIcon.src = 'eye-icon.png'; // Image for showing password
   }
 }
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach((item) => {
+    const question = item.querySelector(".faq-question");
+
+    question.addEventListener("click", () => {
+        item.classList.toggle("active");
+    });
+});
